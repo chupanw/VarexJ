4eebb10eef852e772b6a259c5517f4f3e5e78ac8
chupanw <chupanw@cs.cmu.edu>
2015-09-07 19:13:36 -0400

    Generate test suite for math-bug6b

--- src/tests/cmu/defect4j/MathTestGenerator.java
+++ src/tests/cmu/defect4j/MathTestGenerator.java
@@ -17,9 +17,9 @@ import java.util.LinkedList;
 public class MathTestGenerator extends TestGenerator {
 
     // cpwTODO: set this
-    int version = 14;
+    int version = 6;
     // cpwTODO: set this
-    String jarVersion = "14b";
+    String jarVersion = "6b";
 
     private String config =
 //            "\"+interaction=interaction\"," +
